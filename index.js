59373e8187a9d0c2199e8074690e72771257094a
Henry Amos <amoshenry95@gmail.com>
2024-11-21 03:37:35 +0000

    ➕ feat(api): add CORS to API setup ➕ chore(api): add CORS support ✨ feat(cors): add CORS to API

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import 'dotenv/config';
+import mongoose from "mongoose";
 import connectDB from "./config/db.js";
+import { toJSON } from "@reis/mongoose-to-json";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js"
 import cartRoutes from './routes/cart.js';
@@ -8,10 +11,13 @@ const app = express();
 connectDB()
 //Middleware
 app.use(express.json());
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/product', productRoutes);
+app.use('/api/cart', cartRoutes);
+mongoose.plugin(toJSON);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.use('/api/cart', cartRoutes);
